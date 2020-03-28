cd79c250e285cf03c83fb5b554eff0af1dee4e13
sbake68 <shawnblake@mail.weber.edu>
2020-03-27 18:01:16 -0600

    Final Submission

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -5,3 +5,44 @@ require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 // JavaScript
 //TODO
+import "bootstrap"
+
+const displayRandomBox = function(){
+    let r = Math.random()
+    if (r <0.5){
+        document.write('<div class="red box"></div>')
+    
+    }
+    else{
+        document.write('<div class="blue box"></div>')
+    }
+}
+
+function displayNBoxesUsingFor(n){
+    for(let i = 0; i<n; i++){
+        displayRandomBox()
+       }
+}
+function displayNBoxesUsingWhile(n){
+    let j =0
+    while(j < n){
+        displayRandomBox()
+        j++
+    }
+}
+
+function displayNBoxesUsingDoWhile(n){
+    let k = 0 
+    do{
+       displayRandomBox()
+        k++
+    }while(k<n)
+}
+displayNBoxesUsingFor(15)
+displayNBoxesUsingWhile(12)
+displayNBoxesUsingDoWhile(14)
+
+const square = function(x){
+    return x * x
+}
+alert(square(10))
